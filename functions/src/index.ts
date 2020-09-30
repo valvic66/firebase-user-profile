615f139616e3947c49f689ada5028be43bc3c0be
valvic6 <valvic6@valvic6s-MacBook-Pro.local>
2020-09-30 15:19:30 +0300

    initialized firebase, express server, db, collection and defined the function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,20 @@ import * as admin from 'firebase-admin';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+const main = express();
+
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+
+const db = admin.firestore();
+const userCollection = 'persons';
+
+export const userProfileApi = functions.https.onRequest(main);
+
+
 
 
 
